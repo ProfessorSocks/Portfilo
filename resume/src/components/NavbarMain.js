bf37289ce01b73e69dfc260fcae1768359dd0188
ProfessorSocks <124716422+ProfessorSocks@users.noreply.github.com>
2023-12-20 13:31:33 -0800

    update

--- resume/src/components/NavbarMain.js
+++ resume/src/components/NavbarMain.js
@@ -7,9 +7,9 @@ function NavbarMain() {
     <div className='navbarcontainer'>
         <h2>Camille West</h2>
         <Link to='/Portfilo'><button>Home</button></Link>
-        <Link to='Portfilo/Gallery'><button>Gallery</button></Link>
-        <Link to='Portfilo/Projects'><button>Projects</button></Link>
-        <Link to='Portfilo/Login'><button>Login</button></Link>
+        <Link to='/Portfilo/Gallery'><button>Gallery</button></Link>
+        <Link to='/Portfilo/Projects'><button>Projects</button></Link>
+        <Link to='/Portfilo/Login'><button>Login</button></Link>
     </div>
   )
 }
